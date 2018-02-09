17cead6d2a08faaaa223b06d65de7bc06038e00d
John Emmons <emmons.john@gmail.com>
2018-02-08 23:58:35 -0800

    added LICENSE.txt

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version = "0.0.1",
     author = "John Emmons",
     author_email = "jemmons@cs.stanford.edu",
+    url='https://github.com/jremmons/pyfakewebcam', 
+    license='GPLv3',
     packages=['pyfakewebcam',],
     install_requires = [
         'numpy'
