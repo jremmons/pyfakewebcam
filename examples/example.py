c422d2e6408b6b7bdf50fde7812cfec2ddabd614
John Emmons <emmons.john@gmail.com>
2018-02-08 22:30:56 -0800

    updated exmamples dir

---
+++ examples/example.py
@@ -0,0 +1,27 @@
+import pyfakewebcam
+import scipy.misc as misc
+import numpy as np
+import time
+import timeit
+
+cam = pyfakewebcam.FakeWebcam('/dev/video1', 640, 480)
+
+cam.print_capabilities()
+
+im0 = misc.imread("doge1.jpg")
+im1 = np.zeros((480,640,3), dtype=np.uint8)
+
+while True:
+
+    t1 = timeit.default_timer() 
+    cam.schedule_frame(im0)
+    t2 = timeit.default_timer() 
+    print('write time:{}'.format(t2-t1))
+
+    time.sleep(1/60)
+
+    t1 = timeit.default_timer() 
+    cam.schedule_frame(im1)
+    t2 = timeit.default_timer() 
+    print('write time:{}'.format(t2-t1))
+    time.sleep(1/60)
