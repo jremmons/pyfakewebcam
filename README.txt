37e2c62d56662fdc4ffb1dfdf6fc50fa037dad52
John Emmons <emmons.john@gmail.com>
2018-02-08 22:27:25 -0800

    working code

--- README.txt
+++ README.txt
@@ -1,1 +1,10 @@
 --- pyfakewebcam ---
+
+An API for writing RGB frames to a fake webcam device on Linux!
+
+--- installation ---
+
+$ modprobe v4l2loopback
+$ python setup.py install
+
+
--- pyfakewebcam/pyfakewebcam.py
+++ pyfakewebcam/pyfakewebcam.py
@@ -1,13 +1,11 @@
-
+import cv2
 import os
 import sys
 import fcntl
 import timeit
-import time
 
 import numpy as np
 import pyfakewebcam.v4l2 as _v4l2
-import scipy.misc as misc
 
 class FakeWebcam:
 
@@ -25,6 +23,7 @@ class FakeWebcam:
             sys.stderr.write('sudo modprobe v4l2loopback devices=1\n\n')
             raise FileNotFoundError('device does not exist: {}'.format(video_device))
 
+        self._channels = channels
         self._video_device = os.open(video_device, os.O_WRONLY | os.O_SYNC)
                     
         self._settings = _v4l2.v4l2_format()
@@ -37,6 +36,8 @@ class FakeWebcam:
         self._settings.fmt.pix.sizeimage = width * height * 2
         self._settings.fmt.pix.colorspace = _v4l2.V4L2_COLORSPACE_JPEG
 
+        self._buffer = np.zeros((self._settings.fmt.pix.height, 2*self._settings.fmt.pix.width), dtype=np.uint8)
+        
         fcntl.ioctl(self._video_device, _v4l2.VIDIOC_S_FMT, self._settings)
 
 
@@ -50,75 +51,24 @@ class FakeWebcam:
 
     def schedule_frame(self, frame):
 
-        sys.stderr.write('loading\n')
-        im0 = misc.imread("doge1.jpg")
+        if frame.shape[0] != self._settings.fmt.pix.height:
+            raise Exception('frame height does not match the height of webcam device: {}!={}\n'.format(self._settings.fmt.pix.height, frame.shape[0]))
+        if frame.shape[1] != self._settings.fmt.pix.width:
+            raise Exception('frame width does not match the width of webcam device: {}!={}\n'.format(self._settings.fmt.pix.width, frame.shape[1]))
+        if frame.shape[2] != self._channels:
+            raise Exception('num frame channels does not match the num channels of webcam device: {}!={}\n'.format(self._channels, frame.shape[2]))
 
-        sys.stderr.write('converting\n')
-        buff0 = _convert2YUYV(self._settings.fmt.pix.sizeimage, self._settings.fmt.pix.bytesperline, im0)
+        # t1 = timeit.default_timer()
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2YUV)
+        # t2 = timeit.default_timer()
+        # sys.stderr.write('conversion time: {}\n'.format(t2-t1))        
 
-        sys.stderr.write('loading\n')
-        im1 = np.zeros((self._settings.fmt.pix.height, self._settings.fmt.pix.width, 3))
-        sys.stderr.write('converting\n')
-        t1 = timeit.default_timer()
-        buff1 = _convert2YUYV(self._settings.fmt.pix.sizeimage, self._settings.fmt.pix.bytesperline, im1)
-        t2 = timeit.default_timer()
-        sys.stderr.write('conversion time: {}\n'.format(t2-t1))
+        # t1 = timeit.default_timer()
+        for i in range(self._settings.fmt.pix.height):
+            self._buffer[i,::2] = frame[i,:,0]
+            self._buffer[i,1::4] = frame[i,::2,1]
+            self._buffer[i,3::4] = frame[i,::2,2]                
+        # t2 = timeit.default_timer()
+        # sys.stderr.write('pack time: {}\n'.format(t2-t1))
 
-        sys.stderr.write('looping\n')
-        i = 0 
-        while True:             
-            t1 = timeit.default_timer()
-            if i % 2 == 0:
-                os.write(self._video_device, buff1)
-            else:
-                os.write(self._video_device, buff0)
-                
-            i += 1
-            
-            time.sleep(1/30.0)
-            t2 = timeit.default_timer()
-            sys.stderr.write('time: {}\n'.format(t2-t1))
-
-def _convert2YUYV(sizeimage, bytesperline, im):
-    '''
-    This function is for debugging purposes only!
-    It is too slow to be used for actually runs on the program (3 fps)
-    '''
-    
-    sys.stderr.write('converting_internal\n')
-    buff = np.zeros((sizeimage, ), dtype=np.uint8)
-    imgrey = im[:,:,0] * 0.299 + im[:,:,1] * 0.587 + im[:,:,2] * 0.114
-    Pb = im[:,:,0] * -0.168736 + im[:,:,1] * -0.331264 + im[:,:,2] * 0.5
-    Pr = im[:,:,0] * 0.5 + im[:,:,1] * -0.418688 + im[:,:,2] * -0.081312
-        
-    sys.stderr.write('formating_internal\n')
-    for y in range(imgrey.shape[0]):
-        #Set lumenance
-        cursor = y * bytesperline 
-        for x in range(imgrey.shape[1]):
-            try:
-                buff[cursor] = imgrey[y, x]
-            except IndexError:
-                pass
-            cursor += 2
-
-        #Set color information for Cb
-        cursor = y * bytesperline 
-        for x in range(0, imgrey.shape[1], 2):
-            try:
-                buff[cursor+1] = 0.5 * (Pb[y, x] + Pb[y, x+1]) + 128
-            except IndexError:
-                pass
-            cursor += 4
-
-        #Set color information for Cr
-        cursor = y * bytesperline 
-        for x in range(0, imgrey.shape[1], 2):
-            try:
-                buff[cursor+3] = 0.5 * (Pr[y, x] + Pr[y, x+1]) + 128
-            except IndexError:
-                pass
-            cursor += 4
-
-    return buff.tostring()
-        
+        os.write(self._video_device, self._buffer.tostring())
