0205103b16e1688719b5f7da16e8b66b00093392
John Emmons <emmons.john@gmail.com>
2018-02-09 12:19:36 -0800

    updated version

--- pyfakewebcam/__init__.py
+++ pyfakewebcam/__init__.py
@@ -1,2 +1,3 @@
 
+__version__ = '0.1.0'
 from pyfakewebcam.pyfakewebcam import FakeWebcam
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "pyfakewebcam",
-    version = "0.0.2",
+    version = "0.1.0",
     author = "John Emmons",
     author_email = "mail@johnemmons.com",
     url='https://github.com/jremmons/pyfakewebcam', 
